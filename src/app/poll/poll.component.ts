87fc199f8532badd7f9c898299c086dded812a73
H3rN4n <desouza030689@gmail.com>
2018-10-01 14:18:21 -0300

    some fixes

--- src/app/poll/poll.component.ts
+++ src/app/poll/poll.component.ts
@@ -11,58 +11,18 @@ import {
   selector: 'app-poll',
   templateUrl: './poll.component.html',
   styleUrls: ['./../../assets/css/styles.css', './poll.component.css'],
-  // encapsulation: ViewEncapsulation.Native
+  encapsulation: ViewEncapsulation.Native
 })
 export class PollComponent implements OnInit {
   @Input() poll: Array<any>;
   @Output() sendVotes: EventEmitter<any> = new EventEmitter<any>();
   pollActivePosition: number = 0;
-  // poll: Array<any> = [
-  //   {
-  //     text: 'Lobo esta?',
-  //     type: 'radio',
-  //     active: true,
-  //     answers: [
-  //       {
-  //         text: 'se esta poniendo el pantalon',
-  //         correct: false
-  //       },
-  //       {
-  //         text: 'se esta poniendo el camisa',
-  //         correct: false
-  //       },
-  //       {
-  //         text: 'siiii',
-  //         correct: true
-  //       }
-  //     ]
-  //   },
-  //   {
-  //     text: 'Sal de ahí chivita chivita?',
-  //     type: 'radio',
-  //     active: false,
-  //     answers: [
-  //       {
-  //         text: 'sal de ahi de ese lugar',
-  //         correct: true
-  //       },
-  //       {
-  //         text: 'vente pa ca',
-  //         correct: false
-  //       },
-  //       {
-  //         text: 'siiii',
-  //         correct: false
-  //       }
-  //     ]
-  //   }
-  // ];
 
   constructor() {}
 
   pollActivePositionNext() {
     if (this.poll.length -1 == this.pollActivePosition) {
-      this.vote(this.poll);
+      // this.vote(this.poll);
     } else {
       this.poll[this.pollActivePosition].active = false;
       this.pollActivePosition == this.pollActivePosition++;
@@ -78,7 +38,7 @@ export class PollComponent implements OnInit {
 
   ngOnInit() {}
 
-  vote(poll) {
-    this.sendVotes.emit(poll);
+  vote() {
+    this.sendVotes.emit(this.poll);
   }
 }
